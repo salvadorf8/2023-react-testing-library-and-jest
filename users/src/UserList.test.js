34a993c8e8baf4c0d9cd69b5f003558e215d3470
salvadorf8 <aztecsalvador@gmail.com>
2023-02-05 20:26:53 -0600

    screen.logTestingPlaygroundURL() | EXCELLENT tool - a playground to help you identify elements to use for testing

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -20,7 +20,8 @@ test('render one row per user - preferred way', () => {
 });
 
 // BIG NOTE: Stephen Grider did not like this because - code would need to be modified only
-test('render one row per user  Fallback #1', () => {
+// HOWEVER: it is most preferred
+test('render one row per user - Fallback #1 - using data-testid', () => {
     // Render the component
     const users = [
         { name: 'jane', email: 'jane@gmail.com' },
@@ -42,7 +43,7 @@ test('render one row per user  Fallback #1', () => {
 });
 
 // BIG NOTE: stephen grider likes this version instead
-test('render one row per user Fallback #2', () => {
+test('render one row per user - Fallback #2 - using container.querySelector()', () => {
     // Render the component
     const users = [
         { name: 'jane', email: 'jane@gmail.com' },
@@ -51,12 +52,31 @@ test('render one row per user Fallback #2', () => {
 
     const { container } = render(<UserList users={users} />);
 
-    const table = container.querySelector('table');
-
-    // NICE playground that finds all the rows in a table
-    // screen.logTestingPlaygroundURL();
+    // Find all the tr elements that are inside of the tbody -
+    // use next comment line to disable the red line error
+    // eslint-disable-next-line
     const rows = container.querySelectorAll('tbody tr');
 
     // Assertion: correct number of rows in the table
     expect(rows).toHaveLength(2);
+});
+
+test('render the email and name of each user', () => {
+    // Render the component
+    const users = [
+        { name: 'jane', email: 'jane@gmail.com' },
+        { name: 'sam', email: 'sam@gmail.com' }
+    ];
+
+    render(<UserList users={users} />);
+
+    // to get a little help, use the NICE playground
+
+    for (let user of users) {
+        const name = screen.getByRole('cell', { name: user.name });
+        const email = screen.getByRole('cell', { name: user.email });
+
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 });
