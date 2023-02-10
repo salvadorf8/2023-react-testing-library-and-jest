e69e7518fb011248f79a4bff670a0110eeef0e3e
salvadorf8 <aztecsalvador@gmail.com>
2023-02-10 01:17:26 -0600

    findByRole - MemoryRouter | query used to account for useEffect - fixed act() issue - fixed Link issue

--- codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import RepositoriesListItem from './RepositoriesListItem';
-// 3) in order to pass it, we have to add a MemoryRouter even though we dont need the Link
+// 2) in order to pass it, we have to add a MemoryRouter even though we dont need the Link
 import { MemoryRouter } from 'react-router-dom';
 
-// 8) can also be fixed by skipping the useEffect call completly by
-// pretend of the contents of another file ----- POWERFUL because it did intercept the call to the file
-// and run this instead.... WHAT!!!?!?!?!
+// 8) can also be fixed by skipping the useEffect() call completly by
+// pretend of the contents of another file ----- POWERFUL because it intercepted the call to the file
+// and it ran this instead.... WHAT!!!?!?!?!
 // only use if you dont care about rendering the other component at all
 // jest.mock('../tree/FileIcon', () => {
 //     return () => {
@@ -21,7 +21,7 @@ const renderComponent = () => {
         full_name: 'facebook/react',
         language: 'Javascript',
         description: 'A js library',
-        owner: 'facebook',
+        owner: { login: 'facebook' },
         name: 'react',
         html_url: 'https://github.com/facebook/react'
     };
@@ -35,21 +35,21 @@ const renderComponent = () => {
     return { repository };
 };
 
-// 4) there still an issue - act warning because of a promise.... to fix add async
+// 4) there still an issue - act() warning because of a promise.... to fix add async
 test('shows a link to the github homepage for this repository', async () => {
     // 1) will fail, EXCELLENT example because there is a Link being used in the component
 
     const { repository } = renderComponent();
 
-    // 6) add the following - just to see the data after a pause
+    // 6) add the following - just to see the data after a pause, fixed.... but not ideal
     // screen.debug();
     // await pause();
     // screen.debug();
 
-    // 7) overall!!!!!! this line will fix the act warning and pass your tests
+    // 7) overall!!!!!! this line will fix the act() warning and pass your tests - this is PREFERRED
     await screen.findByRole('img', { name: 'Javascript' });
 
-    // 10) final fix updated renderComponent to return data so that I can grab it here instead of copy paste strings
+    // 10) final fix updated renderComponent to return data so that I can destructure it here instead of copy paste strings
     // 11) added accessible name so that test would fail properly
     const link = screen.getByRole('link', {
         name: /github repository/i
@@ -65,3 +65,23 @@ const pause = () => {
         }, 100);
     });
 };
+
+test('shows a fileicon with the appropriate icon', async () => {
+    renderComponent();
+
+    const icon = await screen.findByRole('img', { name: 'Javascript' });
+
+    expect(icon).toHaveClass('js-icon');
+});
+
+test('shows a link to the code editor page', async () => {
+    const { repository } = renderComponent();
+
+    await screen.findByRole('img', { name: 'Javascript' });
+
+    const link = await screen.findByRole('link', {
+        name: new RegExp(repository.owner.login)
+    });
+
+    expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`);
+});
